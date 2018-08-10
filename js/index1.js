8abf749b6034495c47dc189baa874c0128a095d8
Jānis Iesalnieks <iesalnieks.juniors@gmail.com>
2018-08-10 10:34:43 +0300

    Add index1

---
+++ js/index1.js
@@ -0,0 +1,40 @@
+const validationForm = () => {
+    event.preventDefault();
+    const arrayOfInputs = document.getElementsByTagName("input");
+    let isError = false;
+    const information = {};
+    for (let i = 0; i < arrayOfInputs.length; i++) {
+        const input = arrayOfInputs[i];
+        if (!input.value) {
+            input.style.borderColor = 'red';
+            isError = true;
+        } else {
+            input.style.borderColor = 'initial';
+            isError = false;
+            information[input.name] = input.value;
+        }
+    }
+    return !isError;
+};
+
+var bunchOfUrls = {
+    people :"https://swapi.co/api/people/",
+    planets :"https://swapi.co/api/planets/",
+    spaceships :"https://swapi.co/api/spaceships/"
+};
+
+/**
+ * Function to make an AJAX request by provided parameters and return a string
+ * @param {string} url
+ * @param {string} requestMethod
+ * @param {object|string} body
+ * @returns {string}
+ */
+const sendApiRequest = (url, requestMethod, body = null) => {
+    var xmlHTTP = new XMLHttpRequest();
+    xmlHTTP.open(requestMethod, url, false);
+    xmlHTTP.send(body);
+    return xmlHTTP.responseText;
+};
+
+const Planets = sendApiRequest(bunchOfUrls["planets"], "GET");
