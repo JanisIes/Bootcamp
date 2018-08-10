6a8c91ed879045bc9dee62eefc5066919f427a07
Jānis Iesalnieks <iesalnieks.juniors@gmail.com>
2018-08-10 10:08:26 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const update = data => {
+    $("#films_link").attr("href", data["films"]);
+    $("#people_link").attr("href", data["people"]);
+    $("#planets_link").attr("href", data["planets"]);
+    $("#species_link").attr("href", data["species"]);
+    $("#starships_link").attr("href", data["starships"]);
+    $("#vehicles_link").attr("href", data["vehicles"]);
+
+    // ["films"] = data["films"];
+    // ["people"] = data["people"];
+    // ["planets"] = data["planets"];
+    // ["species"] = data["species"];
+    // ["starships"] = data["starships"];
+    // ["vehicles"] = data["vehicles"];
+};
+
+$.get("https://swapi.co/api/", update);
+
+const loadStarWars = () => {
+    event.preventDefault();
+    console.log(event.target.getAttribute("href"));
+    $(event.target).attr('href');
+
+};
